b50793d498168c3826af82b3afaacdf5e98e34a8
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-27 23:13:56 +0530

    Routers are created and added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { About, Dashboard, Home, Projects, SignIn, SignUp } from './pages'
+
 const App = () => {
   return (
-    <div className="text-3xl">App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
   )
 }
 
