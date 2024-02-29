e691680ff229234dcf7cbf3c21cfadec49e8bce3
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-29 23:24:46 +0530

    created middleware and a function to handle error

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,15 +1,16 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user_model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
     const { userName, email, password } = req.body;
 
-    if(!userName || !email || !password || userName === '' || email === '' || password === '') {
-        return res.status(400).json({message: 'Please fill in all fields'});
+    if (!userName || !email || !password || userName === '' || email === '' || password === '') {
+        next(errorHandler(400, 'Please fill in all fields'));
     }
 
-    if(password.length < 6) {
-        return res.status(400).json({message: 'Password should be atleast 6 characters long'});
+    if (password.length < 6) {
+        next(errorHandler(400, 'Password should be atleast 6 characters long'));
     }
 
     const hashedPassword = await bcryptjs.hash(password, 10);
@@ -24,6 +25,6 @@ export const signUp = async (req, res) => {
         await newUser.save();
         res.status(201).json("user created successfully");
     } catch (error) {
-        res.status(409).json({message: error.message});
+        next(error);
     }
 };
