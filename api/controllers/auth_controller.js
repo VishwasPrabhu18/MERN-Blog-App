5af06b83ddb0e046a1460af4eedbdb0357ad7311
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-03-01 20:30:53 +0530

    Sign in api router is created

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user_model.js";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
     const { userName, email, password } = req.body;
@@ -24,6 +25,36 @@ export const signUp = async (req, res, next) => {
     try {
         await newUser.save();
         res.status(201).json("user created successfully");
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        return next(errorHandler(400, 'Please fill in all fields'));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found'));
+        }
+
+        const validPassword = await bcryptjs.compare(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(400, 'Invalid credentials'));
+        }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+        const { password: userPassword, ...rest } = validUser._doc;
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json(rest);
     } catch (error) {
         next(error);
     }
