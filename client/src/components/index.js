063df8f03462399286d6f445262b5982a85f8501
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-03-04 00:21:25 +0530

    completed sidebar of the dashboard

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,5 @@ export { default as FooterComp } from './FooterComp';
 export { default as OAuth } from './OAuth';
 export { default as ThemeProvider } from './ThemeProvider';
 export { default as PrivateRoute } from './PrivateRoute';
+export { default as DashSidebar } from './DashSidebar';
+export { default as DashProfile } from './DashProfile';
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { DashProfile, DashSidebar } from "../components/index.js";
+
 const Dashboard = () => {
+
+  const location = useLocation();
+
+  const [tab, settab] = useState("");
+
+  useEffect(() => { 
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+
+    if (tabFromUrl) {
+      settab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
-    <div>Dashboard</div>
+    <div className="min-h-screen flex flex-col md:flex-row"> 
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      <div className="">
+        {/* Main Content */}
+        {tab === "profile" && <DashProfile />}
+      </div>
+    </div>
   )
 }
 
