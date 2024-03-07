25619cb4b02c6cedb0254fbe769696bcc5a76a12
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-03-07 14:00:02 +0530

    created getposts api route

--- api/controllers/post_controller.js
+++ api/controllers/post_controller.js
@@ -1,11 +1,11 @@
 import Post from "../models/post_model.js";
 import { errorHandler } from "../utils/error.js";
 
-export const createPost = async (req, res, next) => { 
+export const createPost = async (req, res, next) => {
     if (!req.user.isAdmin) {
         return next(errorHandler(403, "You are not allowed to create a post"));
     }
-    if(!req.body.title || !req.body.content) {
+    if (!req.body.title || !req.body.content) {
         return next(errorHandler(400, "Title and Content are required"));
     }
 
@@ -20,6 +20,36 @@ export const createPost = async (req, res, next) => {
         const savedPost = await post.save();
         res.status(201).json(savedPost);
     } catch (error) {
+        next(errorHandler(500, error.message));
+    }
+};
+
+export const getPosts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === "asc" ? 1 : -1;
+        const posts = await Post.find({
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { category: req.query.category }),
+            ...(req.query.slug && { slug: req.query.slug }),
+            ...(req.query.postId && { _id: req.query.postId }),
+            ...(req.query.searchTerm && {
+                $ord: [
+                    { title: { $regex: req.query.searchTerm, $options: "i" } },
+                    { content: { $regex: req.query.searchTerm, $options: "i" } },
+                ],
+            }),
+        }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
+        
+        const totalPosts = await Post.countDocuments();
+        const now = new Date();
+        const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+        const lastMonthPosts = await Post.countDocuments({ createdAt: { $gte: oneMonthAgo } });
+
+        res.status(200).json({ posts, totalPosts, lastMonthPosts });
+    } catch (error) {
+        console.log(error);
         next(errorHandler(500, error.message));
     }
 };
--- api/routes/post_route.js
+++ api/routes/post_route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from "../utils/verifyUser.js";
-import { createPost } from '../controllers/post_controller.js';
+import { createPost, getPosts } from '../controllers/post_controller.js';
 
 const postRouter = express.Router();
 
 postRouter.post("/create", verifyToken, createPost);
+postRouter.get("/getposts", getPosts);
 
 export default postRouter;
