1e0b1edcdbf4ffcedba41a214c0f3719109fbc1f
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-03-08 22:36:53 +0530

    getUser route is done

--- api/routes/user_route.js
+++ api/routes/user_route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test, updateUser, deleteUser, signOut, getUsers } from "../controllers/user_controller.js";
+import { test, updateUser, deleteUser, signOut, getUsers, getUser } from "../controllers/user_controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const userRouter = express.Router();
@@ -10,5 +10,6 @@ userRouter.put("/update/:userId", verifyToken, updateUser);
 userRouter.delete("/delete/:userId", verifyToken, deleteUser);
 userRouter.post("/signout", signOut);
 userRouter.get("/getusers", verifyToken, getUsers);
+userRouter.get(":userId", getUser);
 
 export default userRouter;
