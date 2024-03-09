4f44177c610f308b9198eb2d0d2810d3427404ce
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-03-09 23:49:20 +0530

    like api route is completed

--- api/routes/comment_route.js
+++ api/routes/comment_route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createComment, getPostComments } from '../controllers/comment_controller.js';
+import { createComment, getPostComments, likeComment } from '../controllers/comment_controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const commentRouter = express.Router();
 
 commentRouter.post('/create', verifyToken, createComment);
-commentRouter.get('/getPostComments/:postId',  getPostComments);
+commentRouter.get('/getPostComments/:postId', getPostComments);
+commentRouter.put("/likeComment/:commentId", verifyToken, likeComment);
 
 export default commentRouter;
