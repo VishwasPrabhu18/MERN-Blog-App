075358767ec2147bb840dd9515dc24b6e18dded6
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-03-12 20:45:12 +0530

    delete comment api is done

--- api/routes/comment_route.js
+++ api/routes/comment_route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createComment, getPostComments, likeComment, editComment } from '../controllers/comment_controller.js';
+import { createComment, getPostComments, likeComment, editComment, deleteComment } from '../controllers/comment_controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const commentRouter = express.Router();
@@ -8,5 +8,6 @@ commentRouter.post('/create', verifyToken, createComment);
 commentRouter.get('/getPostComments/:postId', getPostComments);
 commentRouter.put("/likeComment/:commentId", verifyToken, likeComment);
 commentRouter.put("/editComment/:commentId", verifyToken, editComment);
+commentRouter.delete("/deleteComment/:commentId", verifyToken, deleteComment)
 
 export default commentRouter;
