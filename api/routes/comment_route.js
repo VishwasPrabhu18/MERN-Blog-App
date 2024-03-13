88063e4b8d2b36ae7fbe00f6513ac3490c61f557
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-03-13 21:07:13 +0530

    getcomments is updated

--- api/routes/comment_route.js
+++ api/routes/comment_route.js
@@ -9,6 +9,6 @@ commentRouter.get('/getPostComments/:postId', getPostComments);
 commentRouter.put("/likeComment/:commentId", verifyToken, likeComment);
 commentRouter.put("/editComment/:commentId", verifyToken, editComment);
 commentRouter.delete("/deleteComment/:commentId", verifyToken, deleteComment)
-commentRouter.get("/getcomments", getComments)
+commentRouter.get("/getcomments", verifyToken, getComments)
 
 export default commentRouter;
